a8ed2d9af5cf488c542b9507d556025c249881f9
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-02 13:05:58 +0100

    Initial commit

---
+++ src/app/autocomplete-simple-example.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "autocomplete-simple-example",
+  templateUrl: "autocomplete-simple-example.html"
+})
+export class AutocompleteSimpleExample {
+  options = ["Italie", "France", "Espagne"];
+}
